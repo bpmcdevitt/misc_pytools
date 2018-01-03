631d19c82085ae64c81a3d8188dcfabc8f231f8e
booboy <bpmcdevitt@thelinuxspace.com>
2018-01-03 01:19:27 -0600

    added beginning skeletons of discord_chatbot. lots to learn about asyncio

---
+++ discord_chat_bot/README.md
@@ -0,0 +1,3 @@
+#####Discord Chat Bot
+Chat bot for discord. this will help teach me about asyncio and coroutines in
+python. Developing with discord.py library found [here](https://discordpy.readthedocs.io/en/latest/index.html)
---
+++ discord_chat_bot/discord_chatbot.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# discord chatbot - 100 days of code day 2
+
+import asyncio
+import creds
+import logging
+import discord
+
+# from https://discordpy.readthedocs.io/en/latest/logging.html#logging-setup
+# enable DEBUG mode logging and output to a file named discord.log
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+client = discord.Client()
+
+token = creds.bot_token
+
+
+# login
+async def login(token):
+    await client.login(token)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(login(token))
+loop.close()
