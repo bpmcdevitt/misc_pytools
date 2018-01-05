e99b856c0749fd616721e66b2d5fbdb68f59c36f
booboy <bpmcdevitt@thelinuxspace.com>
2018-01-04 21:29:40 -0600

    took some examples from the discordpy library. looked at some more of the docs of discordpy

--- discord_chat_bot/README.md
+++ discord_chat_bot/README.md
@@ -1,3 +1,7 @@
 #####Discord Chat Bot
 Chat bot for discord. this will help teach me about asyncio and coroutines in
-python. Developing with discord.py library found [here](https://discordpy.readthedocs.io/en/latest/index.html)
+python. Developing with discord.py library found 
+[here](https://discordpy.readthedocs.io/en/latest/index.html)
+
+- copied some of the examples from the discord.py github
+  [here](https://github.com/Rapptz/discord.py/tree/async/examples)
--- discord_chat_bot/discord_chatbot.py
+++ discord_chat_bot/discord_chatbot.py
@@ -14,5 +14,36 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-
 client = discord.Client()
+
+'''
+@asyncio.coroutine
+def main_task():
+    yield from client.login(creds.bot_token)
+    yield from client.connect()
+'''
+
+'''
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(main_task())
+except:
+    loop.run_until_complete(client.logout())
+finally:
+    loop.close()
+'''
+
+@client.event
+async def on_member_join(member):
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await client.send_message(server, fmt.format(member, server))
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(creds.bot_token)
