126a18d41de1fb32c8ba46e082f6161b8a3e237a
booboy <bpmcdevitt@thelinuxspace.com>
2018-01-17 23:16:18 -0600

    added check for str or file

--- json_utils/json_utils.py
+++ json_utils/json_utils.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 # Class that will become a utility to interact easier with json in python
 
+import os
 import json
 from pprint import pprint
 
 class JsonUtils(object):
     """ Json utility library """
 
-    def __init__(self):
-        pass
 
     def decode(self, json_file):
         """ Decode a json string or file that contains json """
-
-        with open(json_file, 'r') as f:
-            data = json.load(f)
+        if os.path.isfile(json_file):
+            with open(json_file, 'r'):
+                data = json.load(json_file)
+                return pprint(data)
+        else:
+            data = json.loads(json_file)
             return pprint(data)
