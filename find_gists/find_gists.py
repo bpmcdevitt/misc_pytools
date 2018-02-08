a5a4495cc5259f59c70136bf17a9665be2aadd68
booboy <bpmcdevitt@thelinuxspace.com>
2018-02-07 19:02:59 -0600

    added a script to find all gists

---
+++ find_gists/find_gists.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# script to enumerate list of github users
+
+import json
+import requests
+import sys
+
+
+username = sys.argv[1]
+
+def find_user_gists(username):
+    r = requests.get(url='https://api.github.com/users/{}/gists'.format(username))
+    json_data = json.loads(r.text)
+    gist_urls = []
+    for i in range(len(json_data)):
+        gist_urls.append(json_data[i]['html_url'])
+    return username, gist_urls
+
+
+if __name__ == '__main__':
+    print(find_user_gists(username))
--- networth/calculate_net_worth.py
+++ networth/calculate_net_worth.py
@@ -2,4 +2,3 @@
 # this is a basic script that will ask for a list of data and spit out networth
 
 # basic - networth = total assets - total liabilities
-
